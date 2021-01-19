8aa5e4e5de62ad51b41e84c7fc10ba166c3d5aed
DevalPanchal2401 <devalp2401@gmail.com>
2021-01-19 10:51:53 -0500

    Added master branch message

--- lab01/lab1.java
+++ lab01/lab1.java
@@ -3,6 +3,8 @@ package lab01;
 public class lab1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+
+		System.out.println("This is a message from the master branch");
 	}
 }
 
