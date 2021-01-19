d275b9349dde65e3b4df3fb4fc5a5b60122d44bc
DevalPanchal2401 <devalp2401@gmail.com>
2021-01-19 10:50:19 -0500

    Added beta branch message

--- lab01/lab1.java
+++ lab01/lab1.java
@@ -3,6 +3,10 @@ package lab01;
 public class lab1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		System.out.println("This is a message from the beta branch");
+		
+		System.out.println("This is a message from the master branch");
 	}
 }
 
