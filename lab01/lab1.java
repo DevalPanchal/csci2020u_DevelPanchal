44b80c653684b3823202557647e9d3d7f77cf2f5
DevalPanchal2401 <devalp2401@gmail.com>
2021-01-26 10:28:51 -0500

    Added new gradle project in lab02 directory

--- lab01/lab1.java
+++ lab01/lab1.java
@@ -3,13 +3,10 @@ package lab01;
 public class lab1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-<<<<<<< HEAD
 
-=======
 		
 		System.out.println("This is a message from the beta branch");
 		
->>>>>>> beta
 		System.out.println("This is a message from the master branch");
 	}
 }
