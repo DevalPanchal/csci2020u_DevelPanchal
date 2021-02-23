856405f3f33643ecf6885aace836239ca73b61a2
DevalPanchal2401 <devalp2401@gmail.com>
2021-02-23 10:44:18 -0500

    Added table view with student records

--- lab05/src/sample/Controller.java
+++ lab05/src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class Controller {
+    @FXML private TableView tableView;
+    @FXML private TableColumn studentIDColumn;
+    @FXML private TableColumn midtermColumn;
+    @FXML private TableColumn assignmentColumn;
+    @FXML private TableColumn finalExamColumn;
+    @FXML private TableColumn finalMarkColumn;
+    @FXML private TableColumn letterGradeColumn;
+
+    private TableView<StudentRecord> studentmarks;
+
+    @FXML
+    public void initialize() {
+        studentIDColumn.setCellValueFactory(new PropertyValueFactory<>("studentID"));
+        midtermColumn.setCellValueFactory(new PropertyValueFactory<>("midterm"));
+        assignmentColumn.setCellValueFactory(new PropertyValueFactory<>("assignment"));
+        finalExamColumn.setCellValueFactory(new PropertyValueFactory<>("finalExam"));
+        finalMarkColumn.setCellValueFactory(new PropertyValueFactory<>("finalMark"));
+        letterGradeColumn.setCellValueFactory(new PropertyValueFactory<>("letterGrade"));
+
+        tableView.setItems(DataSource.getAllMarks());
+    }
 }
