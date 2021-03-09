74b5ff4d334ecff7d6d00db4b8261cde2c873957
DevalPanchal2401 <devalp2401@gmail.com>
2021-03-09 11:29:11 -0500

    Changed title to window output

--- lab06/src/sample/Main.java
+++ lab06/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Graphics in JavaFX");
         primaryStage.setScene(new Scene(root, 1500, 700));
         primaryStage.show();
     }
