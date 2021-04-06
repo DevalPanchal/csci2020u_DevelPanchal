ea4131eb714ad77403e58b16c7920158fdd63e8c
DevalPanchal2401 <devalp2401@gmail.com>
2021-04-05 20:40:06 -0400

    Adding multi threads

--- lab10/src/server/ServerController.java
+++ lab10/src/server/ServerController.java
@@ -3,10 +3,22 @@ package server;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 
+import java.io.IOException;
+
 public class ServerController {
-    @FXML private TextArea inputTextArea;
+    @FXML private static TextArea inputTextArea;
 
-    public void initialize() {
+    public static void setTextArea(TextArea textArea) {
+        ServerController.inputTextArea = textArea;
+    }
 
+    public static TextArea getTextArea() {
+        return ServerController.inputTextArea;
+    }
+
+    public void initialize() throws IOException {
+        setTextArea(inputTextArea);
+        ServerHandler server = new ServerHandler(8080);
+        server.handleRequests();
     }
 }
