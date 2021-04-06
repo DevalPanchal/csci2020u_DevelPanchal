ce6dfd575463d52bbd2957faaab3917e6a9fd567
DevalPanchal2401 <devalp2401@gmail.com>
2021-04-05 20:18:25 -0400

    Changing codebase

--- lab10/src/server/ServerController.java
+++ lab10/src/server/ServerController.java
@@ -1,7 +1,12 @@
 package server;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class ServerController {
+    @FXML private TextArea inputTextArea;
 
+    public void initialize() {
 
-
+    }
 }
